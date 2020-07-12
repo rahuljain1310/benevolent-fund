d44399ba470b1f9a104673ec76ab4f2953d3c2f2
nk554 <nishantkumardubey554@gmail.com>
2020-07-12 23:35:31 +0530

    added carousel

--- frontend/src/Pages/Layout/Layout.js
+++ frontend/src/Pages/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../../Header/Header'
 import Footer from '../Footer/Footer'
 import './Layout.css';
+import ImageCarousel from '../carousel/Carousel';
 
 function CustomLayout () {
 	return(
 		<div className="layout">
 			<Header/>
+			<ImageCarousel />
 			<Footer/>
 		</div>
 	)
