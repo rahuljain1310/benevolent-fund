1324f41919e759d0d01edbdbd9ba53d9dbec9d81
nk554 <nishantkumardubey554@gmail.com>
2020-07-12 23:33:33 +0530

    added carousel

---
+++ frontend/src/Pages/carousel/Carousel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import iitd from '../../Assets/iitd3.jpg';
+
+function ImageCarousel(){
+    return(
+        <div>
+            <img src={iitd} alt="poster" />
+        </div>
+    )
+}
+
+export default ImageCarousel;
