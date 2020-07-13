2e79f38ea6bbce17e3fc29630006572109663131
Rahul Jain <ee1170476@iitd.ac.in>
2020-07-13 15:26:27 +0530

    Refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import ImageCarousel from './Pages/Carousel/Carousel';
 import './App.css';
-import ImageCarousel from './Pages/carousel/Carousel';
 
 function App() {
   return (
