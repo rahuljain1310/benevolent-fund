d28fac02ebbd3d2cafd16856b7f6d0de4c3e36e1
Yashika <63760449+udita17@users.noreply.github.com>
2020-07-14 05:52:00 +0530

    Header done

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import './header.css';
 
 function Header () {
   return(
-    <header className="header">
-    <div class="topnav">
-      <a class="active" href="#home">Home</a>
-      <a href=" ">About</a>
-      <a href=" ">Contribute</a>
+  <header className="header">
+    <img src="D:\IIT-Delhi\AINA\Benevlent Fund\logo.jpg" alt="IITD logo" className="image"></img>
+    <br/><br/><br/><br/><br/>
+    <b><div id="head">IIT DELHI BENEVOLENT FUND</div></b>
+    <i><div id="subhead">Helping others is the way we help ourselves !</div></i><br/>
+    <form>
+      <input type="button" value="HOME" className="buttons" onClick="window.location.href=''"></input>
+      <input type="button" value="ABOUT" className="buttons" onClick="window.location.href=''"></input>
+      <input type="button" value="CONTRIBUTE" className="buttons" onClick="window.location.href=''"></input>
+      <input type="button" value="READ" className="buttons" onClick="window.location.href=''"></input>
+    </form>
+    <div id="para">
+    The <b>IIT Delhi Benevolent Fund</b> is an institutional setup to financially support the <b>campus workers</b><i> (hostel shop workers, night mess workers or washermen etc.) </i>
+    who are not employed under contract. With the campus shut, they no longer have a means of livelihood. These workers help us sustain in the campus by providing
+    services for our daily needs - it is, now, upon the <b>student community to support them, when they most need us! </b>
     </div>
-    <div>
-      <h1>BENEVOLENT FUND IITD </h1>
-      <h2>Helping others is the way we help ourselves</h2>
-      {/* <img src={iitd} alt="iitdimage" className="iitd" /> */}
-    </div>
-    <br /> <br />
-    <p style={{textAlign: "center"}}>
-      The IIT Delhi <span style={{ fontWeight : "bold"}}>Benevolent Fund</span> is an institutional setup to financially 
-      support the campus workers (hostel shop workers, night mess workers or washerman etc) who are not employed under contract. 
-      With the campus shut, they no longer have a means of livelihood. These workers help us sustain in the campus by providing services 
-      for our daily needs- it is, now upon the <span style={{ fontWeight : "bold"}}>student comunity</span> to support them, when they most need us! 
-    </p>
   </header>
   )
 }
