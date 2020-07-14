dddfc2076f410f20debf172bbfdd39ed5b573152
Yashika <63760449+udita17@users.noreply.github.com>
2020-07-14 14:37:05 +0530

    Changed the header beacuse i didnt like it

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -5,19 +5,15 @@ function Header () {
   return(
   <header className="header">
     <img src="D:\IIT-Delhi\AINA\Benevlent Fund\logo.jpg" alt="IITD logo" className="image"></img>
-    <br/><br/><br/><br/><br/>
     <b><div id="head">IIT DELHI BENEVOLENT FUND</div></b>
-    <i><div id="subhead">Helping others is the way we help ourselves!</div></i><br/>
-    <form>
-      <input type="button" value="HOME" className="buttons" onClick="window.location.href=''"></input>
-      <input type="button" value="ABOUT" className="buttons" onClick="window.location.href=''"></input>
-      <input type="button" value="CONTRIBUTE" className="buttons" onClick="window.location.href=''"></input>
-      <input type="button" value="READ" className="buttons" onClick="window.location.href=''"></input>
-    </form>
+    <div id="subhead">Helping others is the way we help ourselves!</div>
+    <a style={{textDecoration:'none'}} href="" className="Links">Home</a>
+    <a style={{textDecoration:'none'}} href="" className="Links">About</a>
+    <a style={{textDecoration:'none'}} href="" className="Links">Contribute</a>
     <div id="para">
-    The <b>IIT Delhi Benevolent Fund</b> is an institutional setup to financially support the <b>campus workers</b><i> (hostel shop workers, night mess workers or washermen etc.) </i>
+    <i>"The <b>IIT Delhi Benevolent Fund</b> is an institutional setup to financially support the <b>campus workers</b> (hostel shop workers, night mess workers or washermen etc.)
     who are not employed under contract. With the campus shut, they no longer have a means of livelihood. These workers help us sustain in the campus by providing
-    services for our daily needs - it is, now, upon the <b>student community to support them, when they most need us! </b>
+    services for our daily needs - it is, now, upon the <b>student community to support them, when they most need us! </b>"</i>
     </div>
   </header>
   )
