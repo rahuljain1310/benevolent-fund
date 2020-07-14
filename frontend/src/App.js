882ae357a37e2f68461f727280b89cc0f97c0154
Rahul Jain <ee1170476@iitd.ac.in>
2020-07-15 00:15:55 +0530

    Minor Changes and Indentation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
-import ImageCarousel from './Pages/Carousel/Carousel';
-import BulletinBoard from './Pages/Bulletinboard/Bulletinboard';
+import ImageCarousel from './Pages/carousel/Carousel';
+import BulletinBoard from './Pages/bulletinboard/Bulletinboard';
 import DisqusComments from './Pages/Comments/Comments';
 import './App.css';
 
 function App() {
   return (
     <div className = "page-container">
-    <div className = "content-wrap">
-      <Header/>
-      <ImageCarousel />
-      <BulletinBoard />
-      <DisqusComments/>
+      <div className = "content-wrap">
+        <Header/>
+        <ImageCarousel />
+        <BulletinBoard />
+        <DisqusComments/>
       </div>
       <Footer/>
     </div>
