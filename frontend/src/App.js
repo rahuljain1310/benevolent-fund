5da6a37e5371fb39ae8de10bbc752159dbc87844
nk554 <nishantkumardubey554@gmail.com>
2020-07-14 19:59:27 +0530

    head removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
-<<<<<<< HEAD
 import ImageCarousel from './Pages/Carousel/Carousel';
 import BulletinBoard from './Pages/Bulletinboard/Bulletinboard';
-=======
 import DisqusComments from './Pages/Comments/Comments';
->>>>>>> d0ae9b6ea335544d202dab18438611279a712c57
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header/>
-<<<<<<< HEAD
       <ImageCarousel />
       <BulletinBoard />
-=======
       <DisqusComments/>
->>>>>>> d0ae9b6ea335544d202dab18438611279a712c57
       <Footer/>
     </div>
   );
