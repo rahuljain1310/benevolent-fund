534e8c6b3419bd4d3ebb6232befd15976c38db6a
Yashika <63760449+udita17@users.noreply.github.com>
2020-07-14 15:25:21 +0530

    Changed the header once again and I have removed the text this time, because i think text can be added in the about page

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './header.css';
+import iitdLogo from '../Assets/iitdLogo.jpg';
 
 function Header () {
   return(
   <header className="header">
-    <img src="D:\IIT-Delhi\AINA\Benevlent Fund\logo.jpg" alt="IITD logo" className="image"></img>
+    <img src={iitdLogo} alt="IIT-D Logo" id="image"></img>
     <b><div id="head">IIT DELHI BENEVOLENT FUND</div></b>
-    <div id="subhead">Helping others is the way we help ourselves!</div>
+    <i><div id="subhead">Helping others is the way we help ourselves!</div></i>
+    <div className="color">
+    <hr/>
     <a style={{textDecoration:'none'}} href="" className="Links">Home</a>
     <a style={{textDecoration:'none'}} href="" className="Links">About</a>
     <a style={{textDecoration:'none'}} href="" className="Links">Contribute</a>
-    <div id="para">
-    <i>"The <b>IIT Delhi Benevolent Fund</b> is an institutional setup to financially support the <b>campus workers</b> (hostel shop workers, night mess workers or washermen etc.)
-    who are not employed under contract. With the campus shut, they no longer have a means of livelihood. These workers help us sustain in the campus by providing
-    services for our daily needs - it is, now, upon the <b>student community to support them, when they most need us! </b>"</i>
+    <hr/>
     </div>
   </header>
   )
