a0611ea52e6fad7be464cf85c439264468047715
Om Agrawal <omagrawal1207@gmail.com>
2020-07-14 18:38:57 +0530

    embedded google form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,17 @@ import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import DisqusComments from './Pages/Comments/Comments';
 import './App.css';
-import FormFeedback from './Pages/FormFeedback/FormFeedback'
+import FormFeedback from './Pages/FormFeedback/FormFeedback';
 
 
 function App() {
   return (
     <div>
       <Header/>
-<<<<<<< HEAD
       <FormFeedback/>
       <div>
         Hello World !
       </div>
-=======
->>>>>>> 559445f135ed948d3e3c8f4fed8cbf999a1fab19
       <DisqusComments/>
       <Footer/>
     </div>
--- frontend/src/Pages/FormFeedback/FormFeedback.js
+++ frontend/src/Pages/FormFeedback/FormFeedback.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FormFeedback () {
   return (
     <div>
-      Hello FormFeedback
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd2SG4Rbm0kM_TxS35vexb4aEFUF37QIGzNzrwL04TgShiCJw/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   )
 }
