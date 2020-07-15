a7982c293404fe87cae67d5f56e4baff6c6188bc
Yashika <63760449+udita17@users.noreply.github.com>
2020-07-15 20:38:56 +0530

    Customisation Done

--- frontend/src/Pages/CommentCard/CommentCard.js
+++ frontend/src/Pages/CommentCard/CommentCard.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {Card} from 'react-bootstrap';
+import './CommentCard.css'
 
 function CommentCard () {
     return(
-        <Card border="dark" style={{ width: '18rem' }}>
-            <Card.Header>
-            <Card.Img variant="top" src="holder.js/100px18" />
-            <Card.ImgOverlay>
-            <Card.Title>Udita Wadhwa</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">First Year Student</Card.Subtitle>
-            </Card.ImgOverlay>
-            </Card.Header>
-            <Card.Body>
-                <Card.Text>
-                My  Experience helping the beneficiaries.......Everyone should come forward.
+        <Card className="CommentCard">
+            <Card.Img variant="top" src="" alt="My Profile Picture" className="image" />
+            <Card.Body className="body">
+                <Card.Text className="title">
+                <b>RAHUL JAIN</b><br/>
+                A Third Year Student, IITD
+                </Card.Text>
+                <Card.Text className="para">
+                "I feel fortunate and happy to play my role in the society especially at a time when it was needed the most."<br/>
+                <a style={{textDecoration:'none'}} href="" className="More">See More...</a>
                 </Card.Text>
             </Card.Body>
         </Card>
         )
     }
-    
+
     export default CommentCard;
