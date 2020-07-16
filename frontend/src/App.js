09317e7b26b9378701a65fd9e4f8c3d596e05f9b
nk554 <nishantkumardubey554@gmail.com>
2020-07-16 06:33:31 +0530

    Newbulletinboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import BulletinBoard from './Pages/bulletinboard/Bulletinboard';
 import DisqusComments from './Pages/Comments/Comments';
 import './App.css';
 import FormFeedback from './Pages/FormFeedback/FormFeedback';
-
+import NewBulletinBoard from './Pages/NewBulletinBoard/BulletinBoard';
 
 function App() {
   return (
@@ -14,7 +14,9 @@ function App() {
       <div className = "content-wrap">
         <Header/>
         <ImageCarousel />
-        <BulletinBoard />
+        {/* <BulletinBoard /> */}
+        <hr style={{width: '80%', backgroundColor:'orange'}}/>
+        <NewBulletinBoard />
         <FormFeedback/>
         <DisqusComments/>
       </div>
---
+++ frontend/src/Pages/NewBulletinBoard/BulletinBoard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import './BulletinBoard.css';
+import CountUp from 'react-countup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+
+function NewBulletinBoard(){
+    const [beneficiary, setBeneficiary] = useState([
+        ["Mukesh","Karakoram shop"],
+        ["Aashish", "Vindy Washerman"],
+        ["Om","Shivalik gardner"],
+        ["Vinay","Himadri shop"],
+        ["Vasant","Kailash juice shop"]]);
+    const [contributors, setContributors] = useState([
+        ["Mukesh",500],
+        ["Aashish", 100],
+        ["Om",1000],
+        ["Vinay",5000],
+        ["Vasant",50],
+        ["Rahul",55],
+        ["Pankaj",250],
+        ["Om",1000],
+        ["Vinay",5000],
+        ["Vasant",50],
+        ["Rahul",55]]);
+    
+    const listOfBeneficiary = beneficiary.map( (person) => 
+                <ListGroup.Item>{person[0]} : {person[1]} 
+                <hr style={{color:"black"}} />
+                 </ListGroup.Item>);
+
+    // const listOfBeneficiary = beneficiary.map( (person) => <li>{person[0]} : {person[1]} <hr style={{color:"black"}} /> </li>);
+    // const listOfContributors = beneficiary.map( (person) => <li>{person[0]} : {person[1]} <hr style={{color:"black"}} /> </li>);
+
+    const listOfContributors = contributors.map( (person, idx) => <ListGroup horizontal key={idx} style={{height:'4rem'}}>
+            <ListGroup.Item style={{width: '300px'}}>
+                {person[0]} 
+                <br/> 
+                <p style={{color:"rgb(49, 6, 6)", fontSize:"12px"}}>1st year undergrad</p>
+                </ListGroup.Item>
+            <ListGroup.Item style={{width: '100px'}}>
+                <p style={{color:"rgb(49, 6, 6)", fontSize:"20px"}}>
+                    {person[1]}
+                    </p>
+                </ListGroup.Item>
+            </ListGroup>);
+
+    const [contributorsCount, setContributorCount]= useState(554);
+
+    const [totalAmount, setTotalAmount] = useState(587554);
+
+
+    return(
+        <Container style={{height:'500px'}}>
+            <Row >
+                <Col md={3}>
+                    <Card border="info"  >
+                        <Card.Header className="text-center" style={{color:'rgb(65, 6, 80)'}}>Total Contributors</Card.Header>
+                        <Card.Body>
+                        <p className="text">
+                        <CountUp  end={contributorsCount} className="counterClass" />
+                        </p>
+                        </Card.Body>
+                    </Card>
+                    <hr />
+                </Col>
+                <Col md={{ span: 3, offset: 0 }}  >
+                    <Card border="info" >
+                    <Card.Header className="text-center" style={{color:'rgb(65, 6, 80)'}}>Total Contribution</Card.Header>
+                    <Card.Body>
+                    <p className="text">
+                    <CountUp  end={totalAmount} />
+                    </p>
+                    </Card.Body>
+                    </Card>
+                    <hr />
+                </Col>
+                
+                <Col md={6} bsPrefix="cold"  style={{overflowY:'scroll', height:'500px', alignItems:'centre', margin:'auto'}}>
+                    <h3 style={{textAlign:'centre', color:'rgb(65, 6, 80)', margin:'auto'}}>Contributors</h3>
+                    <ListGroup>
+                        {listOfContributors}
+                    </ListGroup>
+                    
+                </Col>
+            </Row>
+        </Container>
+)}
+export default NewBulletinBoard;
