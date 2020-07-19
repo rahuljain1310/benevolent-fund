09ba9e0e368abfa7caf6df85f4c1c9c2a30dbda5
Aarushi Gupta <aarushigupta@Aarushis-MacBook-Air.local>
2020-07-20 01:33:57 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-<<<<<<< HEAD
-import BulletinBoard from './Pages/Bulletin/BulletinBoard';
-import DisqusComments from './Pages/Comments/Comments';
-import Beneficiaries from './Pages/Beneficiary/Beneficiaries';
-import About from './Pages/About/About';
-=======
 import About from './Pages/About/About'
 import ImageCarousel from './Pages/Carousel/Carousel';
 import DisqusComments from './Pages/Comments/Comments';
@@ -14,24 +8,10 @@ import FormFeedback from './Pages/FormFeedback/FormFeedback';
 import NewBulletinBoard from './Pages/NewBulletinBoard/BulletinBoard';
 import Beneficiaries from './Pages/NewBeneficiaries/Beneficiaries';
 import CommentPage from './Pages/CommentCard/CommentPage';
->>>>>>> 48b6c4359c86a7fbeaa6d0af4e2c9481f3eacfa6
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="page-container">
-      <Header />
-      <About />
-      <BulletinBoard />
-      <Beneficiaries />
-      <DisqusComments />
-      <Footer />
-    </div>
-  )
-}
-  export default App;
-=======
     <div className = "page-container">
       <div className = "content-wrap">
         <Header/>
@@ -49,11 +29,4 @@ function App() {
   )
 }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 export default App;
-=======
->>>>>>> 239e916738575c87cb00583e897afd19b01339ed
-export default App;
->>>>>>> 48b6c4359c86a7fbeaa6d0af4e2c9481f3eacfa6
--- frontend/src/Pages/CommentCard/CommentPage.js
+++ frontend/src/Pages/CommentCard/CommentPage.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-=======
 import React, {Component} from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 import {
   Container,
   Row,
@@ -10,51 +10,68 @@ import {
 import './CommentCard.css'
 import CommentCard from './CommentCard';
 
-
 class CommentPage extends Component{
-  constructor() {
-    super();
-    this.state = {
-       people: [
-         {
-           name: "Rahul Jain",
-           year: "Third Year, IITD",
-           description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-         },
-         {
-           name: "Om Agrawal",
-           year: "First Year, IITD",
-           description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-         },
-         {
-           name: "Nishant Kumar",
-           year: "First Year, IITD",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."         },
-         {
-           name: "Vipul Garg",
-           year: "First Year, IITD",
-           description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-         }
-       ]
+    constructor() {
+      super();
+      this.state = {
+         people: [
+           {
+             name: "Rahul Jain",
+             year: "Third Year, IITD",
+             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+           },
+           {
+             name: "Om Agrawal",
+             year: "First Year, IITD",
+             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+           },
+           {
+             name: "Nishant Kumar",
+             year: "First Year, IITD",
+             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+           }
+         ]
+      }
+    }
+    render() {
+      let commentcard = this.state.people.map(person => {
+        return(
+          <Col sm="4">
+            <CommentCard person={person}/>
+          </Col>
+        )  
+      })
+      return(
+        <Container fluid>
+          <Row>
+            {commentcard}
+          </Row>
+        </Container>
+      )
     }
   }
-  render() {
-    let commentcard = this.state.people.map(person => {
-      return(
-        <Col sm="3">
-          <CommentCard person={person}/>
-        </Col>
-      )
-    })
+  
+  function  ShareYourStoryButton () {
     return(
-      <Container fluid>
-        <Row>
-          {commentcard}
-        </Row>
-      </Container>
+      <div class='ShareYourStoryButton'>
+        <a href="#" target="blank">
+          &hearts; Share YOUR Experience here
+        </a>
+      </div>
     )
   }
-}
 
-export default CommentPage;
->>>>>>> 239e916738575c87cb00583e897afd19b01339ed
+  function Webpage() {
+    return (
+      <div className = "page-container">
+        <div className = "content-wrap">
+          <Header/>
+
+          <Footer/>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Webpage;
+  
