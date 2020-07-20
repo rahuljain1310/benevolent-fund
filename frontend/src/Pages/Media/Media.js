89548e15c3e2b60870189f8c3a9525d8e5331fc0
nk554 <nishantkumardubey554@gmail.com>
2020-07-20 21:37:41 +0530

    added media card

--- frontend/src/Pages/Media/Media.js
+++ frontend/src/Pages/Media/Media.js
@@ -7,25 +7,43 @@ import {
     CardGroup
 } from 'react-bootstrap';
 import nature1 from '../../Assets/nature2.jpg';
+import './Media.css';
 
 function MediaCard(){
     return(
-        <Container fluid>
+        <div className="mediacard">
+            <h2 style={{textAlign:'center', color:'green', fontWeight:'lighter'}}>IITD Benevolent Fund in News</h2>
+            <hr style={{backgroundColor:'orange', width:'80%'}} />
             <Row>
-                <Col md={4}>
-                    <img src={nature1} style={{height:'200px', alignItems:'center'}}/>
-                </Col>
-                <Col md={8}>
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                    </Card.Body>
-                </Col>
+            <Col md={4} style={{alignItems:'center'}}>
+                <img src={nature1} style={{height:'200px', paddingLeft:'60px'}} />
+            </Col>
+            <Col md={8} style={{alignItems:'center'}}>
+                <h3 style={{color:'blue',fontWeight:'lighter',paddingTop:'10px'}}>Hello</h3>
+                <p>IIT Delhi institutes 'BENEVOLENT FUND' to help people of the lower economic strata within campus, 
+                    receiving voluntary contributions in a formal and transparent manner for meeting minimal needs of 
+                    the people of campus community.</p>
+                <p style={{color:'#737573'}}>- covered by ndtv, 31st March</p>
+                
+            </Col>
             </Row>
-            {/* <Row>
+        </div>
+        // <Container fluid>
+        //     <Row>
+        //         <Col md={6}>
+        //             <img src={nature1} style={{height:'200px', alignItems:'center' }}/>
+        //         </Col>
+        //         <Col md={6}>
+        //             <Card.Body>
+        //             <Card.Title>Card title</Card.Title>
+        //             <Card.Text>
+        //                 This card has supporting text below as a natural lead-in to additional
+        //                 content.{' '}
+        //             </Card.Text>
+        //             </Card.Body>
+        //         </Col>
+        //     </Row>
+            /* <Row>
                 <Col >
                 <CardGroup >
                 <Card>
@@ -80,9 +98,9 @@ function MediaCard(){
                 </Card>
                 </CardGroup>
                 </Col>
-            </Row> */}
+    </Row> */
             
-        </Container>
+        // </Container>
     )
 }
 
