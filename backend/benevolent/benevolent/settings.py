0b17d9cbaa7961b7274db86439fb91105c324ff6
Rahul Jain <ee1170476@iitd.ac.in>
2020-07-23 20:54:37 +0530

    Add .gitignore

--- backend/benevolent/benevolent/settings.py
+++ backend/benevolent/benevolent/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'content',
     'rest_framework',
+    'content',
 ]
 
 MIDDLEWARE = [
@@ -86,14 +86,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
