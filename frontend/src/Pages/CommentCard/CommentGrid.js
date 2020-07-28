44c402c1fa35826f3000d2f4c26a09524476f191
Yashika <63760449+udita17@users.noreply.github.com>
2020-07-28 21:21:22 +0530

    Added 2 more testimonies

--- frontend/src/Pages/CommentCard/CommentGrid.js
+++ frontend/src/Pages/CommentCard/CommentGrid.js
@@ -37,10 +37,16 @@ function CommentGrid() {
       description: "I talked to a rickshaw puller, “They are so happy that words can't measure their emotions. Every amount received was not merely material cash but it brought in with it a lot of emotions.” It's time! when IITians shouldn't be seen as self-centered & career-oriented chaps, We are here to serve humanity. Amounts don’t matter, it's the heart behind that amount which beats and speaks volumes. Let us pledge to not let anyone be vulnerable at least on our campus and stand shoulder to shoulder with the administration to improve the efficacy.We must be grateful for bestowing us with this opportunity to serve society."
     },
     {
-      name: "Nishant Kumar",
+      name: "Vinay Kumar",
       year: "First Year, IITD",
-      imgPath: require('../../Assets/rahul.jpeg'),
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      imgPath: require('../../Assets/vinay.png'),
+      description: "I talked to Ramdas Ji, a rickshaw puller. He told me that he is currently living in Hafizabad, all types of his earnings are stopped due to pandemic. He does not face a shortage of grains but for ration, he seriously needs the money because it is really very difficult for him to manage. He also said that the Rs. 7000 that were sent to him earlier by the student community of IIT Delhi was kind of a lifesaver for him and his family. He was just overwhelmed after he got to know that the “IIT Community” had taken the initiative to help him and he was grateful to IIT for this kind act."
+    },
+    {
+      name: "Bhanu Pratap Aiswal",
+      year: "Second Year, IITD",
+      imgPath: require('../../Assets/bhanu.png'),
+      description: "I interacted with Gopal Tiwari Ji, a helper on our campus. Currently, he is residing in Katwaria Sarai along with his family (3 children and wife). After talking with them, I found that they don’t even have sufficient food since he was unemployed since the beginning of the pandemic. He said that any monetary support will definitely work otherwise whatever destiny gives, he will accept. “To see good times one has to suffer, go through bad times”, he mentioned.He was basically very optimistic that soon things will be fine.In the end, he thanked all the people behind this initiative!"
     }
   ]
   return (
